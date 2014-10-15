5f6a1db9017e63d8baecd7d649cacbc3bd62a45a
Mr. Schifeling <mrSchifeling@eduroam-167-134.dyn.carleton.edu>
2014-10-15 11:07:15 -0500

    Just added a print, but that should be made better probably...

--- task_generator.py
+++ task_generator.py
@@ -8,6 +8,7 @@ import math
 
 task_features = ['x_coord', 'y_coord', 'release_time', 'duration', 'deadline']
 
+
 '''
 Generates a task as a list so that it may be written to a csv file.
 '''
@@ -21,8 +22,10 @@ def generate_task(x_constraint, y_constraint, schedule_length, time_needed, extr
         task[4] = 100
     return task
 
+
 '''
-Given a csv file, it will read tasks into a list before they are to be converted into a class.
+Given a csv file, it will read tasks into a list before they are to be converted into a class
+so each task may then be turned into an object.
 '''
 def read_in_task(csv_file):
     tasks = []
@@ -31,6 +34,7 @@ def read_in_task(csv_file):
         for row in reader:
             tasks.append(row)
     return tasks[1:]
+
 
 # make_objects() is a function which returns a list populated with the information 
 # from the task_list input
@@ -59,41 +63,81 @@ def make_objects(task_list):
 				object_list[i].set_deadline(entry_we_want)
 	return object_list
 
+
 def get_distance(task_a, task_b):
     return ((task_a.x - task_b.x)**2 + (task_a.y - task_b.y)**2)**0.5
 
-def convert_dist_to_time(distance, factor):
-    return distance*factor
-
+'''
+A function given a list of objects and all the potential ways
+to order the tasks will output one of the schedules with the 
+most tasks scheduled.
+'''
 def find_maximal_schedule(object_list, task_orderings):
     max_schedule = []
     for task_ordering in task_orderings:
         new_schedule = create_schedule(task_ordering, object_list)
         if len(new_schedule) > len(max_schedule):
             max_schedule = new_schedule
-            print "This is the new_max_schedule ordering:", max_schedule
     return max_schedule
             
-
+'''
+A function that given an ordering of tasks and a list of objects
+will output the schedule that can be created from that ordering.
+'''
 def create_schedule(task_ordering, object_list):
-    first_task = object_list[task_ordering[0]]
-    schedule = [first_task]
-    ending_time = first_task.release_time + first_task.duration
-    for i in task_ordering[1:]:
-        includable, ending_time = includable_task(ending_time, object_list[i-1], object_list[i])
-        if includable:
-            schedule.append(object_list[i])
+    last_included_task = object_list[task_ordering[0]]
+    schedule = [last_included_task]
+    ending_time = last_included_task.release_time + last_included_task.duration
+    for i in range(len(task_ordering)):
+        if i == 0:
+            pass
+        else:
+            includable, ending_time = includable_task(ending_time, last_included_task, object_list[task_ordering[i]])
+            if includable:
+                last_included_task = object_list[task_ordering[i]]
+                schedule.append(last_included_task)
+                
+                
     return schedule
-    
 
-def includable_task(finishing_time, prev_task, next_task):
+def print_schedule(schedule):
+    finishing_time = 0
+    for i in range(len(schedule)):
+        if i == 0:
+            finishing_time = schedule[i].release_time + schedule[i].duration
+            print "Task", i+1, "will start at", schedule[i].release_time, "and finish at", finishing_time, \
+                "becuase it must start after", schedule[i].release_time, "and must finish before", schedule[i].deadline
+        else:
+            starting_time = get_starting_time_of_next_task(finishing_time, schedule[i-1], schedule[i])
+            finishing_time = starting_time + schedule[i].duration
+            print "Task", i+1, "will start at", starting_time, "and finish at", finishing_time, \
+                "becuase it must start after", schedule[i].release_time, "and must finish before", schedule[i].deadline
+        
+
+
+    
+def get_starting_time_of_next_task(finishing_time, prev_task, next_task):
     distance = get_distance(prev_task, next_task)
-    starting_time = finishing_time + distance
-    ending_time = starting_time + next_task.duration
+    starting_time = max((finishing_time + distance), next_task.release_time)
+    return starting_time
+
+'''
+A function that given the finishing time of the last task done
+and the previous task object and next task object will return
+(true, the end time of finishing the next task) if the next task 
+can be completed before it's deadline and (false, the finishing 
+time of the task previously included) if the next_task cannot be finished.
+'''
+def includable_task(finishing_time, prev_task, next_task):
+    starting_time = get_starting_time_of_next_task(finishing_time, prev_task, next_task)
+    ending_time = (starting_time + next_task.duration)
     if ending_time <= next_task.deadline:
         return True, ending_time
     return False, finishing_time
 
+'''
+a function that returns all potential orderings of tasks.
+'''
 def get_all_permutations(length_of_perms):
     permutations = [i for i in range(length_of_perms)]
     permutations = list(itertools.permutations(permutations, length_of_perms))
@@ -116,7 +160,7 @@ def main():
     object_list = set_up_N_tasks()
     task_orderings = get_all_permutations(len(object_list))
     best_schedule = find_maximal_schedule(object_list, task_orderings)
-    print best_schedule
+    print_schedule(best_schedule)
 
 
 
