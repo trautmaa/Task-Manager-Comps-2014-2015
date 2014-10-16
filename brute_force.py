357871b049be29d209269a23c9a040096f6e242d
Mr. Schifeling <mrSchifeling@eduroam-167-134.dyn.carleton.edu>
2014-10-16 10:59:00 -0500

    Added a helper function that I assume more than one algorithm will use called helper_functions.py: It needs to be commented.

--- brute_force.py
+++ brute_force.py
@@ -8,12 +8,10 @@ import itertools
 import math
 from create_tasks_from_csv import *
 from write_to_csv import *
+from helper_functions import *
 
 task_features = ['x_coord', 'y_coord', 'release_time', 'duration', 'deadline']
 
-
-def get_distance(task_a, task_b):
-    return ((task_a.x - task_b.x)**2 + (task_a.y - task_b.y)**2)**0.5
 
 '''
 A function given a list of objects and all the potential ways
@@ -47,27 +45,7 @@ def create_schedule(task_ordering, object_list):
                 
                 
     return schedule
-
-def print_schedule(schedule):
-    finishing_time = 0
-    for i in range(len(schedule)):
-        if i == 0:
-            finishing_time = schedule[i].release_time + schedule[i].duration
-            print "Task", i+1, "will start at", schedule[i].release_time, "and finish at", finishing_time, \
-                "becuase it must start after", schedule[i].release_time, "and must finish before", schedule[i].deadline
-        else:
-            starting_time = get_starting_time_of_next_task(finishing_time, schedule[i-1], schedule[i])
-            finishing_time = starting_time + schedule[i].duration
-            print "Task", i+1, "will start at", starting_time, "and finish at", finishing_time, \
-                "becuase it must start after", schedule[i].release_time, "and must finish before", schedule[i].deadline
-        
-
-
     
-def get_starting_time_of_next_task(finishing_time, prev_task, next_task):
-    distance = get_distance(prev_task, next_task)
-    starting_time = max((finishing_time + distance), next_task.release_time)
-    return starting_time
 
 '''
 A function that given the finishing time of the last task done
@@ -83,10 +61,10 @@ def includable_task(finishing_time, prev_task, next_task):
         return True, ending_time
     return False, finishing_time
 
+
 '''
 a function that returns all potential orderings of tasks.
 '''
-
 def get_all_permutations(length_of_perms):
     permutations = [i for i in range(length_of_perms)]
     permutations = list(itertools.permutations(permutations, length_of_perms))
