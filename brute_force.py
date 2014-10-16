f72de1287a52898773031bd8a55acc3b7fb58b50
Mr. Schifeling <mrSchifeling@eduroam-167-134.dyn.carleton.edu>
2014-10-16 11:32:57 -0500

    finished commenting brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -14,9 +14,9 @@ task_features = ['x_coord', 'y_coord', 'release_time', 'duration', 'deadline']
 
 
 '''
-A function given a list of objects and all the potential ways
-to order the tasks will output one of the schedules with the 
-most tasks scheduled.
+A function given a list of task objects and all the potential ways
+to order the tasks will create a schedule for each ordering
+and output one of the schedules with the most tasks scheduled.
 '''
 def find_maximal_schedule(object_list, task_orderings):
     max_schedule = []
@@ -25,7 +25,8 @@ def find_maximal_schedule(object_list, task_orderings):
         if len(new_schedule) > len(max_schedule):
             max_schedule = new_schedule
     return max_schedule
-            
+   
+         
 '''
 A function that given an ordering of tasks and a list of objects
 will output the schedule that can be created from that ordering.
@@ -38,11 +39,11 @@ def create_schedule(task_ordering, object_list):
         if i == 0:
             pass
         else:
-            includable, ending_time = includable_task(ending_time, last_included_task, object_list[task_ordering[i]])
+            includable, ending_time = includable_task(ending_time, last_included_task, 
+                                                      object_list[task_ordering[i]])
             if includable:
                 last_included_task = object_list[task_ordering[i]]
                 schedule.append(last_included_task)
-                
                 
     return schedule
     
@@ -63,25 +64,27 @@ def includable_task(finishing_time, prev_task, next_task):
 
 
 '''
-a function that returns all potential orderings of tasks.
+a function that returns all potential orderings of tasks as a list.
+It just takes a number of how many elements you want to be permutated.
 '''
 def get_all_permutations(length_of_perms):
     permutations = [i for i in range(length_of_perms)]
     permutations = list(itertools.permutations(permutations, length_of_perms))
     return permutations
 
-
-
-
-def main():
-    written_csv  = write_n_tasks(10, 'my.csv')
+'''
+a function that will run our brute force algorithm to find one of the
+best schedules.
+'''
+def run_brute_force_alg(n):
+    written_csv  = write_n_tasks(n, 'my.csv')
     object_list = get_object_list(written_csv)
     task_orderings = get_all_permutations(len(object_list))
     best_schedule = find_maximal_schedule(object_list, task_orderings)
     print_schedule(best_schedule)
 
-
-
+def main():
+    run_brute_force_alg(10)
 
 
 if __name__ == '__main__':
