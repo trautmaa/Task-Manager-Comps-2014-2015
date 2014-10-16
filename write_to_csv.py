6269ee447688c8faac2bdee49e0ca9c39afce17f
Mr. Schifeling <mrSchifeling@William-Schifelings-MacBook-Pro.local>
2014-10-15 22:40:51 -0500

    finished my comments on write_to_csv.py

--- write_to_csv.py
+++ write_to_csv.py
@@ -1,10 +1,9 @@
 # Comps 2014
 # Larkin Flodin, Avery Johnson, Maraki Keteman, 
 # Abby Lewis, Will Schifeling, and  Alex Trauman
+
 import csv
 import random
-import math
-
 
 task_features = ['x_coord', 'y_coord', 'release_time', 'duration', 'deadline']
 
@@ -13,18 +12,21 @@ Generates a task as a list so that it may be written to a csv file.
 Each parameter is the range for that feature... Ie the task's x coordinate
 will randomly be between 0 and x_constraint. The task that is returned is:
 [x_coord, y_coord, release_time, duration, deadline] (where deadline is
-release time + duration + some random extra_time
+release time + duration + some random extra_time)
 '''
 def generate_task(x_constraint, y_constraint, schedule_length, time_needed, extra_time):
     features = [x_constraint, y_constraint, schedule_length, time_needed, extra_time]
     task = []
     for feature in features:
         task.append(random.random()*feature)
-    task[4] = task[2] + task[3] + task[4]
+    task[4] = task[2] + task[3] + task[4] #deadline = release_time + duration + extra time
     return task
 
 '''
-A function that 
+A function that will write n tasks to a csv file.  It uses
+generate_task to create the task to write. Right now the
+constraints for generate_task are hard coded but that can 
+be changed.  The name of the csv file is returned.
 '''
 def write_n_tasks(n, csv_file):
     with open(csv_file, 'wb') as f:
