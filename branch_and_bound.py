c2303794521aed313f1da4a002bc6ecb81ac22b9
Mr. Schifeling <mrSchifeling@William-Schifelings-MacBook-Pro.local>
2014-10-28 23:26:25 -0500

    added a branch_and_bound.py file with a bit of a start.

---
+++ branch_and_bound.py
@@ -0,0 +1,53 @@
+# Comps 2014
+# Larkin Flodin, Avery Johnson, Maraki Keteman, 
+# Abby Lewis, Will Schifeling, and  Alex Trautman
+
+
+from helper_functions import *
+
+class Node(object):
+    def __init__(self):
+        self.children = []
+        self.parent = []
+        self.schedule = None
+        self.searched = 0
+        self.tasks_to_search = None
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def add_parent(self, parent):
+        self.parent.append(child)
+
+    def set_schedule(self, schedule):
+        self.schedule = schedule
+
+    def mark_searched(self):
+        self.searched = 1
+
+    def set_tasks_to_search(self, tasks_to_search):
+        self.tasks_to_search = tasks_to_search
+
+def compute_lower_bound():
+    return 10000 # To be changed
+
+def choose_best_node(node_a, node_b):
+    if len(node_a.schedule) > len(node_b.schedule):
+        return node_a
+    return node_b
+
+def dfs(node, upper_bound_node):
+    if len(node.tasks_to_search) == 0:
+        return choose_best_node(node, upper_bound_node)
+
+    return upper_bound_node
+        
+    
+
+
+def main():
+    node = Node()
+
+
+if __name__ == "__main__":
+    main()
