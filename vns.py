abb69dd9e9de8ca19c8cbbcfb3d25315902ee924
lewisab <lewisab@carleton.edu>
2014-11-07 17:16:08 -0600

    Created vns.py and skeleton code within. To be continued...

---
+++ vns.py
@@ -0,0 +1,35 @@
+def vns(G, currSolution, stoppingCondition):
+    return currSolution
+
+def shaking(currSolution, nHoodIndex):
+    return currSolution
+
+def optionalExchange1(currSolution, nHoodIndex):
+    return currSolution
+
+def optionalExchange2(currSolution, nHoodIndex):
+    return currSolution
+
+def crossExchange(currSolution, nHoodIndex):
+    return currSolution
+
+def iterativeImprovement(G, currSolution, nHoodIndex):
+    return currSolution
+    
+def isBetter(sol1, sol2):
+    return True
+
+def isFeasible(G, currSolution):
+    return True
+
+def tightenTWStarts(G, currSolution):
+    return G
+
+def tightenTWEnds(G, currSolution):
+    return G
+
+def minRoute(G, currSolution):
+    return []
+
+def calcDominantSolution(leaveTimes):
+    return leaveTimes
