e4741ce8e72c2df0498de94e70814b4da221a6ae
Alex Trautman <trautmaa@carleton.edu>
2014-11-13 17:30:48 -0600

    Created directory with PuLP file

---
+++ IntegerProgrammingPuLP/taskManagerIntProg.py
@@ -0,0 +1,27 @@
+'''
+Task Manager Comps
+
+Advisor:
+David Liben-Nowell
+
+Group members:
+Larkin Flodin, Avery Johnson, Maraki Ketema,
+Abby Lewis, Will Schifeling, Alex Trautman
+
+This program uses Python's PuLP - a linear programming libray - to model and solve an integer program that describes our base problem.
+'''
+
+from pulp import *
+
+'''
+Objective function problem variable created to contain problem data
+'''
+
+prob = LpProblem("Objective Function",LpMinimize)
+
+
+
+
+
+
+
