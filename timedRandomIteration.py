bd328ac3ba15da2ffed0a0f47919945ac7659833
Larkin Flodin <flodinl@carleton.edu>
2015-01-24 19:52:20 -0600

    Cleaned up timedRandomIteration a bit

--- timedRandomIteration.py
+++ timedRandomIteration.py
@@ -3,37 +3,45 @@
 # Abby Lewis, Will Schifeling, and  Alex Trautman
 
 
-import itertools
-
 import createTasksFromCsv
 import helperFunctions
 import time
 import random
 
-from Objects import Schedule, Route
-
+'''
+Reads a list of tasks from a file, and tries to create schedules using
+randomly selected permutations of tasks. Stops and
+returns the best schedule after numSeconds seconds have passed.
+'''
 def randomlyPickBestScheduleUnderTime(csvFile, numSeconds):
+    numberIterations = 0
     taskList = createTasksFromCsv.getTaskList(csvFile)
     helperFunctions.preprocessTimeWindows(taskList)
     
     bestSchedule, bestProfit = None, 0
-    numTasks = len(taskList)
-    taskIds = []
-    for i in range(numTasks):
-        taskIds.append(taskList[i].id)
+    taskIds = [task.id for task in taskList]
     
     start = time.time()
-    while time.time()< (start + numSeconds):
+    while time.time() < (start + numSeconds):
+        # Get a random permutation
         orderToTry = random.sample(taskIds, len(taskIds))
         schedule = helperFunctions.createOptimalSchedule(taskList, orderToTry)
         
         if schedule.getProfit() > bestProfit:
             bestProfit = schedule.getProfit()
             bestSchedule = schedule
+        numberIterations += 1
+    print
+    print numberIterations
+    print
     return bestSchedule
     
 def main():
-    print randomlyPickBestScheduleUnderTime("test.csv", 20)
+    schedule = randomlyPickBestScheduleUnderTime("test.csv", 15)
+    print schedule
+    print
+    print "priority is: "
+    print schedule.getProfit()
     
-    
-main()
+if __name__ == "__main__":
+    main()
