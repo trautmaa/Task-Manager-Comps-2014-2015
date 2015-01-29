0185487313b568943248a77824bfaa0013f43b8c
Mr. Schifeling <mrSchifeling@eduroam-167-143.dyn.carleton.edu>
2015-01-28 22:13:14 -0600

    Fixed the csv tests to actually be team orienteering problems. We can't directly compare our tests to there's because they have mandatory starting locations with 0 priority which I input as mandatory locations with lots of priority... So we best get more profit than them.

--- Already Created Tests/readInTests.py
+++ Already Created Tests/readInTests.py
@@ -11,7 +11,7 @@ def readInTask(csvFile):
     with open(csvFile, 'rU') as f:
         for row in f:
             tasks.append(row.split())
-    return tasks[2:-1] # tasks[0] is the list of names of the features.
+    return tasks[:-1] # tasks[0] is the list of names of the features.
 
 #i 0 = vertex number
 #x 1= x coordinate
@@ -26,19 +26,35 @@ def readInTask(csvFile):
    
 def writeFromReadInTasks(inputCSVFile, outputCSVFile):
     taskList = readInTask(inputCSVFile)
+    numDays = int(taskList[0][1])
+    dayLength = float(taskList[2][8]) + 5
+    taskList = taskList[2:]
     with open(outputCSVFile, 'wb') as f:
         writer = csv.writer(f)
         writer.writerow(taskFeatures)
         first = True
         for task in taskList:
             if first:
-                newTask = [task[1], task[2], 0, 0, 0.1, 1, 1, [[(0, 0.1)]]]
-                writer.writerow(newTask)
-                newTask = [task[1], task[2], float(task[8]), 0, float(task[8])+0.1, 1, 1, [[(float(task[8]), float(task[8]) + 0.1)]]]
-                first = False
+                for day in range(numDays):
+                    depotStart = 0 + day*dayLength
+                    depotEnd = 0.1 + day*dayLength
+                    newTask = [task[1], task[2], depotStart, 0, depotEnd, 1, 1, [[(depotStart, depotEnd)]]]
+                    writer.writerow(newTask)
+                    depotStart = float(task[8]) + day*dayLength
+                    depotEnd = float(task[8])+0.1 + day*dayLength
+                    newTask = [task[1], task[2], depotStart, 0, depotEnd, 1, 1, [[(depotStart, depotEnd)]]]
+                    writer.writerow(newTask)
+                    first = False
             else:
-                newTask = [task[1], task[2], task[8], task[3], (float(task[8]) + float(task[3])), task[4], 0, [[(float(task[8]), float(task[8])+float(task[3]))]]]
-            writer.writerow(newTask)
+                days = []
+                for day in range(numDays):
+                    depotStart = float(task[8]) + day*dayLength
+                    depotEnd = float(task[9])+float(task[3]) + day*dayLength
+                    day = [(depotStart, depotEnd)]
+                    days.append(day)
+                    
+                newTask = [task[1], task[2], float(task[8]), task[3], depotEnd, task[4], 0, days]
+                writer.writerow(newTask)
     
-for i in range(8):
-    writeFromReadInTasks("50_rc10" + str(i+1) + ".txt", "50_rc10" + str(i+1) + ".csv")
+for i in range(9):
+    writeFromReadInTasks("50_c10" + str(i+1) + ".txt", "50_c10" + str(i+1) + ".csv")
